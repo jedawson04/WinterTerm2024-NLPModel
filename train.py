17389d464a2b779904821f469c9992079632e003
Ezra Crowe <ecrowe@oberlin.edu>
2024-01-16 21:18:14 -0800

    Did a thing I think... Hasnt really finished running yet

--- train.py
+++ train.py
@@ -1,7 +1,11 @@
-import yfinance as yf
 from fastai.text.all import *
 
-# Fetch stock data for SPY from 2019 to the current date
-stock_data = yf.download('SPY', start='2019-01-01', end='2023-12-28')
+dls = TextDataLoaders.from_folder(untar_data(URLs.IMDB), valid='test')
 
-print(stock_data)
+learn = text_classifier_learner(dls, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
+
+learn.fit_one_cycle(1, 2e-2)
+
+learn.save('sentiment_model')
+
+learn.predict("I really loved that movie, it was awesome!")
